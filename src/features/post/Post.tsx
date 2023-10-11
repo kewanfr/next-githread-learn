12841a755ca785bb59ea5790cb4f83dd3e52aad6
Kéwan Bernier <mail@kewan.fr>
2023-10-11 22:07:28 +0200

    /posts url edit

--- src/features/post/Post.tsx
+++ src/features/post/Post.tsx
@@ -2,7 +2,7 @@ import { PostHome } from '@/src/query/post.query'
 import React from 'react'
 import { PostLayout } from './PostLayout'
 import Link from 'next/link'
-import { Button } from '@/components/ui/button'
+import { Button, buttonVariants } from '@/components/ui/button'
 import { Heart, MessageCircle } from 'lucide-react'
 
 type PostProps = {
@@ -12,7 +12,7 @@ export const Post = ({post}: PostProps) => {
 
   return (
     <PostLayout user={post.user} postId={post.id} createdAt={post.createdAt}>
-      <Link href={`/post/${post.id}`} className='text-sm text-foreground'>
+      <Link href={`/posts/${post.id}`} className='text-sm text-foreground'>
         {post.content}
       </Link>
 
@@ -21,9 +21,9 @@ export const Post = ({post}: PostProps) => {
       <Button size='icon' variant='ghost'>
         <Heart size={20} />
       </Button>
-      <Button size='icon' variant='ghost'>
+      <Link className={buttonVariants({variant: 'ghost', size: 'icon'})} href={`/posts/${post.id}/reply`}>
         <MessageCircle size={20} />
-      </Button>
+      </Link>
       </div>
       <div>
         <Link href={`/posts/${post.id}`} className='text-sm text-muted-foreground'>
