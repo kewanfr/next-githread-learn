52195cebd3503e461d3e200f5341949251293ecf
Kéwan Bernier <mail@kewan.fr>
2023-10-11 22:43:14 +0200

    profile and user profile follow

--- app/profile/page.tsx
+++ app/profile/page.tsx
@@ -1,9 +1,46 @@
 // localhost:3000/profile
 
+import { getAuthSession } from '@/lib/auth';
+import { getUserProfile } from '@/src/query/user.query';
+import { notFound } from 'next/navigation';
 import React from 'react'
+import Profile from '../users/[userId]/Profile';
+import { Button, buttonVariants } from '@/components/ui/button';
+import { Post } from '@/src/features/post/Post';
+import { followUser } from '../users/[userId]/follow.action';
+import Link from 'next/link';
+import { PenSquare } from 'lucide-react';
 
-export default function Profile() {
+export default async function ProfilePage() {
+  const session = await getAuthSession();
+
+  if(!session?.user?.id) {
+    notFound();
+  }
+
+  const user = await getUserProfile(session?.user?.id);
+  if(!user) {
+    notFound();
+  }
   return (
-    <div>Profile</div>
-  )
+    <div>
+      
+    <Profile user={user}>
+      <form className="mt-4">
+        <Link href='/profile/edit' className={buttonVariants({
+          variant: 'outline',
+        })}
+        >
+          Edit Profile
+        </Link>
+      </form>
+    </Profile>
+    <div className="divide-y divide-accent border-t border-accent mt-6">
+      {user.posts.map((post) => (
+        <Post post={post} key={post.id} />
+      ))}
+    </div>
+    </div>
+
+  );
 }
--- app/users/[userId]/page.tsx
+++ app/users/[userId]/page.tsx
@@ -1,13 +1,19 @@
-import { getAuthSession } from '@/lib/auth'
-import { getUserProfile } from '@/src/query/user.query';
-import React from 'react'
-import Profile from './Profile';
-import { notFound } from 'next/navigation';
+import { getAuthSession } from "@/lib/auth";
+import { getUserProfile } from "@/src/query/user.query";
+import React from "react";
+import Profile from "./Profile";
+import { notFound, redirect } from "next/navigation";
+import { prisma } from "@/lib/prisma";
+import { Button } from "@/components/ui/button";
+import { followUser } from "./follow.action";
+import { Post } from "@/src/features/post/Post";
 
-export default async function UserPage({params}: {
-  params:{
-    userId: string
-  }
+export default async function UserPage({
+  params,
+}: {
+  params: {
+    userId: string;
+  };
 }) {
   const session = await getAuthSession();
   const user = await getUserProfile(params.userId);
@@ -16,7 +22,48 @@ export default async function UserPage({params}: {
     return notFound();
   }
 
+  const isFollowing = session?.user.id
+    ? await prisma.follow.findFirst({
+        where: {
+          followerId: session?.user.id,
+          followingId: user.id,
+        },
+        select: {
+          id: true,
+        },
+      })
+    : null;
+
+  const isCurrentUser = params.userId === session?.user.id;
+
+  if (isCurrentUser) {
+    redirect("/profile");
+  }
+
   return (
-    <Profile user={user} />
-  )
+    <div>
+      
+    <Profile user={user}>
+      <form className="mt-4">
+        <Button
+          variant="outline"
+          formAction={async () => {
+            "use server";
+            if (!session?.user.id) return;
+
+            await followUser(params.userId);
+          }}
+        >
+          {isFollowing ? "Unfollow" : "Follow"}
+        </Button>
+      </form>
+    </Profile>
+    <div className="divide-y divide-accent border-t border-accent mt-6">
+      {user.posts.map((post) => (
+        <Post post={post} key={post.id} />
+      ))}
+    </div>
+    </div>
+
+  );
 }
